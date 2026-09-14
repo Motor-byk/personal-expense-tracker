73bcfd77b927a0533f10fc47ed7f74047ee2a6e9
Motor-byk <211898162+Motor-byk@users.noreply.github.com>
2026-09-14 16:58:14 -0700

    Implement conditional rendering for expand
    -on button press, the expand button will show weekly expenses component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,21 @@
 import Topbar from "./components/Topbar";
 import WeeklyExpenses from "./components/WeeklyExpenses";
 import Bottominput from "./components/Bottominput";
+import { useState } from "react";
 
 function App() {
-
     //temp
     const tempweeklyexpense = [
                                 {name: "Costco Hotdog", cost: 150},
                                 {name: "Steam Frame", cost: 105900},
                                 {name: "Feet Pics", cost: 69696700}];
+
+    const [expanded, setExpanded] = useState(false);
+
+
+    function handleExpand() {
+        setExpanded(!expanded);
+    }
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -17,8 +24,8 @@ function App() {
                 <h2 className="text-2xl font-bold">Weekly Balance</h2>
                 <h3 className="text-xl font-bold text-green-800">$100</h3>
                 <div>
-                    <button className="text-sm text-blue-950 underline italic">Expand</button>
-                    <WeeklyExpenses expenses={tempweeklyexpense}/>
+                    <button onClick={handleExpand} className="text-sm text-blue-950 underline italic">Expand</button>
+                    {expanded && <WeeklyExpenses expenses={tempweeklyexpense}/>}
                 </div>
             </main>
             <Bottominput/>
