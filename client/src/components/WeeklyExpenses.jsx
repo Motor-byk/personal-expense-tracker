c9b91cc419fbc391359458a7c1ee794b4b0ba8d2
Motor-byk <211898162+Motor-byk@users.noreply.github.com>
2026-09-16 19:47:23 -0700

    Add basic css to WeeklyExpenses
    -Colored the balance and expenses
    -Aligned prices left and names right
    -Added total spend across week

--- client/src/components/WeeklyExpenses.jsx
+++ client/src/components/WeeklyExpenses.jsx
@@ -3,11 +3,22 @@ import { toStringCents } from "../util/expenseCalc";
 export default function WeeklyExpenses({expenses = [], startingBalance = 0}){
         return (
         <div className="overflow-y-auto overflow-x-auto max-h-80 max-w-md">
-            <ul>
-                <li key="startingBalance">{toStringCents(startingBalance)}</li>
+            <ul className="w-50">
+                <li className="flex justify-between"> 
+                    <span className="italic text-green-700">{toStringCents(startingBalance)}</span>
+                    <span className="italic">Starting Balance</span>    
+                </li>
                 {expenses.map((expense, index) => {
-                    return <li key={index}><span>{toStringCents(expense.cost)}</span> <span>{expense.name}</span></li>
+                    return <li className="flex justify-between" key={index}>
+                                <span className="text-red-700">-{toStringCents(expense.cost)}</span> 
+                                <span className="">{expense.name}</span>
+                            </li>
                 })}
+                <hr class="border-t border-gray-300 my-2" />
+                <li className="flex justify-between">
+                    <span class="underline font-semibold text-red-800">{toStringCents(expenses.reduce((total, cur) => total + cur.cost, 0))}</span>
+                    <span>Total Spent</span>
+                </li>
             </ul>
         </div>
     );
